51cc0cc3c4919d52bd6c290cc4e51ec7beb2a54f
Witalis Domitrz <witekdomitrz@gmail.com>
2022-12-29 13:29:51 +0000

    d24

---
+++ d24.py
@@ -0,0 +1,134 @@
+# %%
+d = """
+#.########################################################################################################################
+#<^vvv<v^v>^vvv.v<vv<>^^<^><.vvv^>v<^<<<<^>^vv^>^<^>>v>v^v^>^v>vv>^.^..v<^<^^^>>>><<>v<^^^v<>>><>.vv^vv>v.vv<<^<.v...^v.<#
+#<^^><><^^^<^>>v^^>v<^vvvv><vv<v.<vv^v<>v^.v^>v^v<>^<>^<vv.v^.^<<<^^^..<.><<v<vv^<v^^>>.><v<vv.>v>><<v.v^<^<v^^^^vv^v>^v>#
+#<^.>>v<<vv^^<<<<<.<v<>^>v^v<v<<<^<v^<<vvvv>v<^v<^v<>v.<>v^^^.^vv.<vv>v<^^v^>>.><^.><<<>v^>><^.<><>v^vv>>^v>>.><vvv^<<>v<#
+#>v^v<>^<.^.^>><>>..<vv<>^>>vv<.<.<^v<^<>>><<^v.v^<v<vv><v.<v.>v^v^<.v>^v^<^.v>.v.>^<v^>vv><<v.>^>v><<^vv<v^>v>v>.>v>>><.#
+#><<<^<v.><>>^>v<^><^<^v.^v<vv<<^^vvv><^^>>>>v^.^^v^<v^^.vv^v^<.v<v^^v<^..>>v<^v^.v^^^^.>v^<vv^<<>><<>vv><^<<^^><..vv.v^.#
+#<^>^.^v.><^>.v<v<>^<^<<<<<^^><vv.>^v><^<.>>.><^<v>>>^^^vvv<>v^^^<.^v^<^v<v>^.^.<>v^.v>v<v^<^^vv.^<><>v^>>.^>>><>><>v..^<#
+#>.^>^>^.v<vv.<>.>>v<>><^vv>>^<<v^.><^^>^^^.<><><.<v<<^.^<><<^>v<v<v<v>^^<^<v<<...<<>v<v<>.^v>>^^^^<<<><v<^>.^v^.>v<v>v.>#
+#<.>.^.<>^^<^v<<^^^^v..<><>^vvv<^>..v<>^vvvv<<<>v^<v.v^<<>v.<^v^>^>>vv<.<<.<^^^v^v<vv<.>^<^.v.<<<v.v^v>v^^<vv^^^>v>v^>><>#
+#<v>v>^<vvv^v<^>^v>.<<^v^^>v>^v>^.<<v<vv<<v^<v<^vvv.<>.<>^vv.>^vvv<v<><v^.<^v<..><<><^v..v>^>v<>>.<<>^<^v>v.^><<^v<<.v^>>#
+#<<<^<<^v^vv><><<>vv<>...<^<<<vv^^^...<><<<^vv^<vv<^<v>^<^^^^>v^>>.<^<^<>>v^><><v^v>>><v>^^<^.>><><^<v<^.>>v^>^v^v<v^^^<<#
+#<>.><^v.vv><.v<v>^.>^><<^<><vv>^v<^>v<^.v^.v<>^.>^>vv>^^>>v<^><^v.>v^>^vvv^^<v<>..v^^<>..v^>v^^^<<>vv^vv>v><<v<.>><<vvv>#
+#><><.^^<<<<.<<><v^<v>^>^>vv..v>^v<<v^<^^.<<>^>>.<>>^<<v.^>^^vv>^^v.^<<.>>><v^>.>>^.>>^<^v>^<>v<v.v<.<^.v>^<>v^v>.^.><^v>#
+#><>>>>^<>>>v^v^v^vv.<^^.^.^>^v>>v<<^..^>><<.^<v><>v>>><<>vv.<>v.^<^^<v^vv^>v<>v><^^vv>vvv.v><v.<<<<>>>vv>v.>v>>^^v>.^<><#
+#<<><><^>^v<>v^.^<.^<v^<v.>.>v^v.<^><<vv<vvv^<>^>.v>>v^><><<<v<.^v>><vv<^v<>^v>v.^<^.><.^<.v^v<^^><>^.^>v>.>^<..<<^>^vv..#
+#><v>v.v<>>v<<><>>^<>vv^<<>^vvv^<>^>>v><^<v.<^<<<^><<<<<^.^v><><<><><v.vv^^v.<<<<>.<<>v^.<^.v<v>>v^<..^><v<<^^<^>^^v^>.<>#
+#.<<>>>v.<>^<<^v.^<>v<><vv>.^v.^>.<v.>v<.^vvv>v^<<v<><^<v^.>vv^.<<^v^v>>vvv>.>>^>^<<<v.>>.vv>>>^v^vv^v<v<<^>>><.<>>^>v^<<#
+#<<>><><>>^v>^>.<v^v..v.v^v^>.><^.><^<>^.>>v.^>vv^v><^..<>^vv<>.<<.<>v^^..>^<>v><<^>^v><<v>..>vvv<.>>v<v<vv<^^v>^v<v^v<v.#
+#>vvv.><<<^<>>><<<v><>>>>^v<<.<><vvv.<<^^v.v<>^<>^v.^^>.<v>v>v^v^^<^.v^>^><v>^>>^<>>vvvv<.vv^v<^v^^^.^>^><><^vvv.>.v^<<v>#
+#>vv.><>>^.^v<.>vv>>.^v^vv>^v^>>><v<<vv.><^><>.><>>><^<>v^v<>^<<<^>^>^<.><.v>><v<>v>>><vv^.>^v.>v^<>v>v^vv^<v<<^v^v<<^>^>#
+#<v^vvv<><<<vv.<^v<<v^^>v^<vvv^^vvv^^>v<.<>v^>>>>^>.^><^>^^<^<^.v>>>v>^<<<v>>.v^v<v><^>>v.<^>^v^vv>>^^vv<<v<v<<vv^<^<.vv>#
+#<^>v^^^^vv><>>^>v>^>v<vv<><v.v<^vv><v<^^^^^.<<><v<><^.>^^.<^v>^<v>^<<.v>>^v^^.^<.<v>.^><.>^v^^v<><v<.v<.>>><^>^vv<.^.>><#
+#<>^^vvv<<v^vv<<>v>.^<>v^>.vv<vv^>.<^>v>v^>.^v<.<>^<vv^v><^v.^v>^^>v^>>><>vv<>vv>.^v><>.<^^v<<<^>^^^><>^^>^.^>vv<.<><<^>>#
+#<<^^..>v>vv<v<>>v<><^<v<v^>.<>v>^^>^v<.v^.<>>.<<>>v^><<^^<>^.<v^vvv^v^<<<^v.^vv^^^<v><<<v^<<>^.<><vvv^<>v<^>.>>><>^v<>v<#
+#<>>^.>^^^vv<>^>^<v.^^v><.v<^<vv>^^v><<>v<>>>>>.<^^vv^^^>.<<>v^>>.^.<v^>.>.v^vv>><>.><<^^>>v^>>>^>v<>.^^v>^>vvv^^^^.^^^.<#
+#>v<vvv<^^>v^>>^^<vvvv.v.v>^<v^>>v><><<v^.<><v<^>v.<^v<>>^v<.^><^v>^^>v^<v^v>>^vv><v.^<><<v<<<v^v>vv><<^<>v^<<^...<<.<>^<#
+########################################################################################################################.#
+""".strip().splitlines()
+
+directions = ((-1, 0), (1, 0), (0, 1), (0, -1), (0, 0))
+blizzard_directions = "^v><"
+blizzards_init_lists = [[] for _ in blizzard_directions]
+Mx, My = len(d) - 1, len(d[0]) - 1
+entrance = (0, 1)
+goal = (Mx, My - 1)
+
+for i, row in enumerate(d):
+    for j, v in enumerate(row):
+        if v in blizzard_directions:
+            blizzards_init_lists[blizzard_directions.find(v)].append((i, j))
+blizzards = [frozenset(dir_blizzards) for dir_blizzards in blizzards_init_lists]
+
+
+def teleport_blizzard(x, y):
+    if x <= 0:
+        x = Mx - 1
+    if x >= Mx:
+        x = 1
+    if y <= 0:
+        y = My - 1
+    if y >= My:
+        y = 1
+    return x, y
+
+
+def blizzards_step(blizzards):
+    new_blizzards = []
+    for (dx, dy), direction_blizzards in zip(directions, blizzards):
+        new_blizzards.append(
+            frozenset(teleport_blizzard(x + dx, y + dy) for x, y in direction_blizzards)
+        )
+    return new_blizzards
+
+
+def possible_positions_from(pos, all_blizzards):
+    res = []
+    x, y = pos
+    for dx, dy in directions:
+        nx, ny = x + dx, y + dy
+        if (nx, ny) in (entrance, goal):
+            res.append((nx, ny))
+        if not (0 < nx < Mx and 0 < ny < My):
+            continue
+        if (nx, ny) not in all_blizzards:
+            res.append((nx, ny))
+    return res
+
+
+def perform_one_step(all_pos, blizzards):
+    blizzards = blizzards_step(blizzards)
+    all_blizzards = frozenset.union(*blizzards)
+    new_all_pos = []
+    for pos in all_pos:
+        new_all_pos += possible_positions_from(pos, all_blizzards)
+    return frozenset(new_all_pos), blizzards
+
+
+def blizzards_to_str(blizzards, all_pos):
+    board = [
+        ["#" if i == 0 or i == Mx or j == 0 or j == My else "." for j in range(My + 1)]
+        for i in range(Mx + 1)
+    ]
+    board[entrance[0]][entrance[1]] = "E"
+    board[goal[0]][goal[1]] = "G"
+    for c, dir_blizzards in zip(blizzard_directions, blizzards):
+        for x, y in dir_blizzards:
+            if board[x][y] == ".":
+                board[x][y] = c
+            elif board[x][y].isdecimal():
+                board[x][y] = str(int(board[x][y]) + 1)
+            else:
+                board[x][y] = "2"
+    for x, y in all_pos:
+        board[x][y] = "x"
+    return "\n".join("".join(row) for row in board)
+
+
+all_pos = frozenset([entrance])
+res = 0
+while True:
+    if goal in all_pos:
+        break
+    all_pos, blizzards = perform_one_step(all_pos, blizzards)
+    res += 1
+p1_res = res
+
+print(p1_res)
+all_pos = frozenset([goal])
+while True:
+    if entrance in all_pos:
+        break
+    all_pos, blizzards = perform_one_step(all_pos, blizzards)
+    res += 1
+
+all_pos = frozenset([entrance])
+while True:
+    if goal in all_pos:
+        break
+    all_pos, blizzards = perform_one_step(all_pos, blizzards)
+    res += 1
+p2_res = res
+print(p2_res)
